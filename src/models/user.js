c471bdd4538d5907c569c7bb7789a102c4d8977f
Nikhilsunny123 <nikhilsunny35@gmail.com>
2023-11-24 19:48:10 +0530

    add

--- src/models/user.js
+++ src/models/user.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
+  email: { type: String, required: true, },
   fullName: { type: String, required: true },
-  password: { type: String, required: true },
+  role: { type: String, required: true },
+  password: { type: String, required: true, select: false },
 });
 
 const Users = mongoose.model("Users", userSchema);
